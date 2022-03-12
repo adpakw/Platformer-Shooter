ffc286b30fda8ba34c93b9b03f9c102428513765
adpakw <94380412+adpakw@users.noreply.github.com>
2022-03-12 16:15:20 +0300

    Keyboard input

---
+++ main.py
@@ -0,0 +1,112 @@
+import pygame
+pygame.init()
+
+screen_width = 720
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height), 1)  # creat a graphical window (создаем графическое окно)
+pygame.display.set_caption("Shooter Platformer")  # set the current window caption (задаем заголовок окна)
+
+background = pygame.image.load("1618553103_55-funart_pro-p-oboi-fon-sploshnoi-chernii-tsvet-63.jpg", )
+# set framerate (установим частоту кадров)
+clock = pygame.time.Clock()
+FPS = 60
+
+moving_left = False
+moving_right = False
+
+section_size = 36  # define the size of sections (задаем размер ячеек)
+
+def draw_grid():
+    for line in range(0, 20):
+        pygame.draw.line(screen, (255, 255, 255), (0, line * section_size), (screen_width, line * section_size))
+        pygame.draw.line(screen, (255, 255, 255), (line * section_size, 0), (line * section_size, screen_height))
+
+
+world_data = [
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1],
+[1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1],
+[1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1],
+[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1],
+[1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, char_type, x, y, scale, speed):
+        pygame.sprite.Sprite.__init__(self)
+        self.char_type = char_type
+        self.speed = speed
+        self.direction = 0
+        self.flip = False
+        img = pygame.image.load('img/player/Idle/0.png')
+        self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def move(self, moving_left, moving_right):
+        dx = 0
+        dy = 0
+
+        if moving_left:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def draw(self):
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
+player = Player("player", 200, 200, 1, 5)
+enemy = Player("enemy", 400, 200, 2, 5)
+
+
+while True:
+    screen.blit(background, (0, 0))
+
+    player.draw()
+    enemy.draw()
+    clock.tick(FPS)
+    draw_grid()
+    player.move(moving_left, moving_right)
+
+    pygame.display.update()  # update portions of the screen for software displays (обновляем наше окно)
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
+
+        # quit game (выйти из игры)
+        if event.type == pygame.QUIT:
+            pygame.quit()
